3e2026b6b24309418145144b877575fb47c1d7b6
Gloria Pappalardo <60324395+grpappalardo@users.noreply.github.com>
2020-10-29 21:04:10 -0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,65 @@
+# Gloria Pappalardo
+# 10/29/20
+# This code masks the globe save New Zealand then runs an NDVI over the country using historical data. NDVI, normalized difference vegetation index, shows vegetation density 
+# varying across the year with the seasons.
+
+var col = ee.ImageCollection('MODIS/006/MOD13A2').select('NDVI');
+// Define a mask to clip the NDVI data by.
+var mask = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017')
+  .filter(ee.Filter.eq('country_co', 'NZ'));
+
+// Define the regional bounds of animation frames.
+var x = ee.Geometry.Polygon(
+  [[[161.48299451272322, -33.39238530088807],
+  [161.48299451272322, -48.66006479475098],
+  [185.12557263772322, -48.66006479475098],
+  [185.12557263772322, -33.39238530088807]]], 
+null, false);
+
+col = col.map(function(img) {
+  var doy = ee.Date(img.get('system:time_start')).getRelative('day', 'year');
+  return img.set('doy', doy);
+});
+var distinctDOY = col.filterDate('2013-01-01', '2014-01-01');
+// Define a filter that identifies which images from the complete collection
+// match the DOY from the distinct DOY collection.
+var filter = ee.Filter.equals({leftField: 'doy', rightField: 'doy'});
+
+// Define a join.
+var join = ee.Join.saveAll('doy_matches');
+
+// Apply the join and convert the resulting FeatureCollection to an
+// ImageCollection.
+var joinCol = ee.ImageCollection(join.apply(distinctDOY, col, filter));
+// Apply median reduction among matching DOY collections.
+var comp = joinCol.map(function(img) {
+  var doyCol = ee.ImageCollection.fromImages(
+    img.get('doy_matches')
+  );
+  return doyCol.reduce(ee.Reducer.median());
+});
+// Define RGB visualization parameters.
+var visParams = {
+  min: 0.0,
+  max: 9000.0,
+  palette: [
+    'FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901',
+    '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01',
+    '012E01', '011D01', '011301'
+  ],
+};
+
+// Create RGB visualization images for use as animation frames.
+var rgbVis = comp.map(function(img) {
+  return img.visualize(visParams).clip(mask);
+});
+// Define GIF visualization parameters.
+var gifParams = {
+  'region': x,
+  'dimensions': 600,
+  'crs': 'EPSG:3857',
+  'framesPerSecond': 10
+};
+
+// Print the GIF URL to the console.
+print(rgbVis.getVideoThumbURL(gifParams));
